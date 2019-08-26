cd03767a7332f9103b29119e12ec0b211325cef9
Connor Wolfe <Connor@Connors-MacBook-Pro-2.local>
2019-08-26 17:47:46 -0400

    Set up request handler with 404 and 200 codes, Made the file reader place the data into the listingData file, and set up the server and it's messages

--- server.js
+++ server.js
@@ -11,41 +11,57 @@ var requestHandler = function(request, response) {
 
   /*
     Your request handler should send listingData in the JSON format as a response if a GET request 
-    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
+    is sent to the '/listings' path. Otherwise, it should send a 404 error.  */
 
-    HINT: Explore the request object and its properties 
+  if (parsedUrl.pathname === '/listings') {
+    response.writeHead(200, {'Content-Type' : 'application/json'});
+    response.write(JSON.stringify(listingData)); //something needed on the end here maybe?
+    response.end();
+  } else {
+    response.writeHead(404, {'Content-Type' : 'text/plain'});
+    response.write('Bad gateway error')
+    response.end();
+  }
+   /* HINT: Explore the request object and its properties 
     HINT: Explore the response object and its properties
     https://code.tutsplus.com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
-    http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
+    http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation */
     
-    HINT: Explore how callback's work 
-    http://www.theprojectspot.com/tutorial-post/nodejs-for-beginners-callbacks/4
+   /* HINT: Explore how callback's work 
+    http://www.theprojectspot.com/tutorial-post/nodejs-for-beginners-callbacks/4 */
     
-    HINT: Explore the list of MIME Types
+   /* HINT: Explore the list of MIME Types
     https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
    */
+  
 };
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
-  /*
+    /*
     This callback function should save the data in the listingData variable, 
-    then start the server. 
+    then start the server.
+    */
 
-    HINT: Check out this resource on fs.readFile
+    //HINT: Check out this resource on fs.readFile
     //https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
 
-    HINT: Read up on JSON parsing Node.js
-   */
-
+    //HINT: Read up on JSON parsing Node.js
+    //*/
+    //Data  is the contents of the file.
+    //listingData = data;
+    listingData = JSON.parse(data);
+    console.log(listingData);
     //Check for errors
+    if (err) throw err;
   
 
-   //Save the sate in the listingData variable already defined
   
 
-  //Creates the server
-  
+    //Creates the server
+    server = http.createServer(requestHandler);
   //Start the server
+    server.listen(8080);
+    console.log('server listening on http://localhost:8080');
 
 
 });
